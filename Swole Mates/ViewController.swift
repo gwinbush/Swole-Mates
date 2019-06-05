4ac9b158550c48d54ae007a849e26136cae5d724
Gabriel Winbush <gabewinbush@gmail.com>
2019-06-04 20:54:03 -0400

    Initial commit

---
+++ Swole Mates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swole Mates
+//
+//  Created by Gabriel Winbush on 6/4/19.
+//  Copyright © 2019 Gabriel Winbush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swole MatesTests/Swole_MatesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swole_MatesTests.swift
+//  Swole MatesTests
+//
+//  Created by Gabriel Winbush on 6/4/19.
+//  Copyright © 2019 Gabriel Winbush. All rights reserved.
+//
+
+import XCTest
+@testable import Swole_Mates
+
+class Swole_MatesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
